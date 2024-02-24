321557ef16d12b809412958656751db269b5c932
Juan Roberto Dejon <jrodejon@gmail.com>
2024-02-25 00:31:51 +0800

    asd

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,16 +1,20 @@
 <template>
-  <form @submit.prevent="registerUser">
-    <label for="username">Username:</label>
-    <input v-model="username" type="text" id="username" required />
+  <div class="container">
+    <form @submit.prevent="registerUser">
+      <div class="form-container">
+        <label for="username">Username:</label>
+        <input v-model="username" type="text" id="username" required />
 
-    <label for="email">Email:</label>
-    <input v-model="email" type="email" id="email" required />
+        <label for="email">Email:</label>
+        <input v-model="email" type="email" id="email" required />
 
-    <label for="password">Password:</label>
-    <input v-model="password" type="password" id="password" required />
+        <label for="password">Password:</label>
+        <input v-model="password" type="password" id="password" required />
 
-    <button type="submit">Register</button>
-  </form>
+        <button type="submit">Register</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -24,15 +28,28 @@ export default {
   },
   methods: {
     registerUser() {
-      // Assuming you want to pass username and email to the /profile route
       const userData = {
         username: this.username,
         email: this.email
       }
 
-      // Pass userData as query parameters
       this.$router.push({ name: 'profile', query: userData })
     }
   }
 }
 </script>
+
+<style>
+div.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+div.form-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: start;
+}
+</style>
